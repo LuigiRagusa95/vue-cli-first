6083192a59abd2978de34ee2c88432266d5bacb8
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-22 14:23:47 +0100

    init project and first vue components

---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+	<h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+	name: "Title",
+	data() {
+		return {
+			title: "La mia prima app con Vue CLI!",
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+	font-size: 2rem;
+	line-height: 2.5rem;
+}
+</style>
